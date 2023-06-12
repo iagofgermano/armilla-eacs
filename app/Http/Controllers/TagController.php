9b66163df59873fb254df90fffb64bad1c217245
Iago Germano <iagog2005@gmail.com>
2023-06-11 23:00:05 -0300

    creating endpoints for tag register on db

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tag;
 
 class TagController extends Controller
 {
-    //
+    public function index(){
+        return response()->json([
+            "allowed" => true
+        ]);
+    }
+
+    public function create(Request $request){
+        
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TagController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/tag', [TagController::class, 'index']);
+
+Route::post('/tag/create', [TagController::class, 'create']);
