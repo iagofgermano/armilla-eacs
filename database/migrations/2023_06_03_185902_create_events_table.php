6ce4c521aaae9490735478fb1eba9631d78023cb
Iago Germano <iagog2005@gmail.com>
2023-06-24 16:42:22 -0300

    modifying migrations for all tables

--- database/migrations/2023_06_03_185902_create_events_table.php
+++ database/migrations/2023_06_03_185902_create_events_table.php
@@ -11,8 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('owners', function (Blueprint $table) {
+        Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->boolean('active')->default(true);
+            $table->foreignId('owner_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('owners');
+        Schema::dropIfExists('events');
     }
 };
--- database/migrations/2023_06_04_211500_create_tags_table.php
+++ database/migrations/2023_06_04_211500_create_tags_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('tag')->unique();
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('event_id')->nullable()->constrained()->nullOnDelete();
             $table->timestamps();
         });
     }
