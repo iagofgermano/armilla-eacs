4d8e7ee259a61c03873defb21bbde905c48b80b7
Iago Germano <iagog2005@gmail.com>
2023-07-06 19:33:50 -0300

    user and owner homepages

--- routes/web.php
+++ routes/web.php
@@ -15,26 +15,60 @@ use App\Http\Controllers\UserController;
 |
 */
 
+// view routes
+
 Route::get('/', function () {
     return view('home');
 });
 
+//owner routes
+
 Route::get('/owners/login', function () {
+    if(session()->has('name')){
+        return redirect('/owners');
+    }
     return view('owners.login');
 });
 
 Route::get('/owners/register', function () {
+    if(session()->has('name')){
+            return redirect('/owners');
+        }
     return view('owners.register');
 });
 
+Route::get('/owners', function () {
+    return view('owners.home');
+});
+
+// user routes
+
 Route::get('/users/login', function () {
+    if(session()->has('username')){
+        return redirect('/users');
+    }
     return view('users.login');
 });
 
 Route::get('/users/register', function () {
+    if(session()->has('username')){
+        return redirect('/users');
+    }
     return view('users.register');
 });
 
- //Route::post('/users/submit', [UserController::register]);
+Route::get('/users', function () {
+    return view('users.home');
+});
 
-//Route::post('/owners/submit', [OwnerController::register]);
+
+// register routes
+Route::post('/users/submit', [UserController::class, 'register']);
+
+Route::post('/owners/submit', [OwnerController::class, 'register']);
+
+//login routes
+
+Route::post('/users/login', [UserController::class, 'auth']);
+
+Route::post('/owners/login', [OwnerController::class, 'auth']);
