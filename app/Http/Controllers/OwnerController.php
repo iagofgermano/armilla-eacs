6c4ea750a640ffd7aefb76bfa113825ebfd83887
Iago Germano <iagog2005@gmail.com>
2023-07-07 19:06:05 -0300

    functions for authentication and registry

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -3,8 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Owner;
+use Illuminate\Support\Facades\Auth;
 
 class OwnerController extends Controller
 {
-    //
+    public function auth(Request $request, Owner $owner){
+        $credentials = $request->validate([
+            'name' => ['required'],
+            'password' => ['required']
+        ]);
+
+        $data = $request->input();
+        
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            
+            $request->session()->put('name', $data['name']);
+
+            return redirect()->intended('/owners');
+        }
+ 
+        return back()->withErrors([
+            'name' => 'The provided credentials do not match our records.',
+        ])->onlyInput('name');
+    }
+
+    public function register(Request $request, Owner $owner){
+        $data = $request->input();
+
+        $owner->name = $data['name'];
+        $owner->email = $data['email'];
+        $owner->phone = $data['phone'];
+        $owner->password = $data['password'];
+
+        $owner->save();
+
+        $request->session()->put('name', $data['name']);
+
+        return redirect('/owners');
+    }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    public function auth(Request $request, User $user){
+        
+        $credentials = $request->validate([
+            'username' => ['required'],
+            'password' => ['required']
+        ]);
+
+        $data = $request->input();
+        
+        if (Auth::attempt($credentials)) {
+            
+            $request->session()->regenerate();
+            
+            $request->session()->put($data);
+
+            return redirect()->intended('/users/'.$data['username']);
+        }
+ 
+        return back()->withErrors([
+            'username' => 'The provided credentials do not match our records.',
+        ])->onlyInput('username');
+    }
+    
+
+    public function register(Request $request, User $user){
+        $data = $request->input();
+
+        $user->username = $data['username'];
+        $user->email = $data['email'];
+        $user->password = $data['password'];
+
+        $user->save();
+
+        $request->session()->put('username', $data['username']);
+
+        return redirect('/users');
+    }
 }
