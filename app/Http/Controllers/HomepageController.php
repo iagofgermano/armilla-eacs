f5ad505c12583e620accfb2c5a8b64d808df03b8
Iago Germano <iagog2005@gmail.com>
2023-07-12 18:53:36 -0300

    getting events for owners funcitonality

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -4,7 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Event;
+use App\Models\Owner;
+
 class HomepageController extends Controller
 {
+    public function render(Request $request, String $username){
+        if(session()->has('username')){
+            if($username == session()->get('username')){
+    
+                $events = Event::where('active', 1)
+                    ->orderBy('name')
+                    ->get();
+
+                return view('users.home', ['events' => $events]);
+
+            }
+        }
+        return view('users.login');
+    }
+
+
+    public function getEvents(Request $request, String $name){
+        if(session()->has('name')){
+            if($name == session()->get('name')){
+
+                $events = Owner::where('name',$name)->first()->events;
+
+                
+                
+                return view('owners.home', ['events' => $events]);
+            }
+        }
+        return view('owners.login');
+    }
+
     
 }
