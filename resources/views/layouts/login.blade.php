0ac352c5190f28408d1aad3f57879bfc3ea9a580
armillaeacs-laptop <iagog2005@gmail.com>
2023-09-04 19:41:05 -0300

    adding initial css for homepage and login

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -4,22 +4,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title')</title>
+    <title>@yield('ArmillaEACS - Login<')</title>
+    <link rel="stylesheet" type="text/css" href="/css/login.css">
 </head>
 <body>
-
     <header>
-        <a href="/">HOME</a>
-        <a href="/owners/login">Para empresas</a>
-        <a href="/users/login">Para usuários</a>
-    </header>
+        <nav class="navbar">
+          <a class="logo" href="#">ArmillaEACS</a>
+          <ul class="nav-links">
+            <li><a href="/owners/login">Para empresas</a></li>
+            <li><a href="/users/login">Para usuários</a></li>
+          </ul>
+          <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+          </div>
+        </nav>
+      </header>
 
     <main>
         <h1>@yield('body-title')</h1>
         <form action="@yield('action')" method="post">
+            <section class="login-form">
+                <div class="login-box">
+                    <h2>Login</h2>
             @csrf
+            <div class="user-box">
             @yield('fields')
-            <button type="submit">Enviar</button>
+         </div>
+            <button class="btn" type="submit">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Iniciar
+        </button>
+    </div>
+</section>
             <p>
                 <a href="/">Voltar</a>
             </p>
@@ -27,7 +49,7 @@
     </main>
 
     <footer>
-        ArmillaEACS &copy; 2023.
+        <p>&copy; 2023 ArmillaEACS - Early Access Control System</p>
     </footer>
 </body>
 </html>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,16 +4,70 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title')</title>
+    <title>@yield('ArmillaEACS - Entry Access Control System<')</title>
+    <link rel="stylesheet" type="text/css" href="/css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-l22Up6v3U6dQOik9J4RgtG8GVvswbvN6UwT6Tb+6zYSv6q3dCQ/9X4vjRIV8sNxbU+/SPmxFYwm7GhCcWQ2L7Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-
     <header>
-        <a href="/">HOME</a>
-        <a href="/owners/login">Para empresas</a>
-        <a href="/users/login">Para usuários</a>
-    </header>
-    @yield('content')
+        <nav class="navbar">
+          <a class="logo" href="#">ArmillaEACS</a>
+          <ul class="nav-links">
+            <li><a href="/owners/login">Para empresas</a></li>
+            <li><a href="/users/login">Para usuários</a></li>
+          </ul>
+          <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <section class="hero">
+          <div class="container">
+            <h2>Controle de Fluxo de Pessoas em Eventos</h2>
+            <p>Com a ArmillaEACS, gerencie o acesso de visitantes, controle vendas e estoque e crie uma experiência personalizada para seus clientes.</p>
+            <a href="#" class="btn">Experimente Agora</a>
+          </div>
+        </section>
+        <section class="features">
+          <div class="container">
+            <h3>Recursos Principais</h3>
+            <ul>
+              <li><i class="fas fa-check"></i> Controle de fluxo de pessoas</li>
+              <li><i class="fas fa-check"></i> Controle de vendas e estoque</li>
+              <li><i class="fas fa-check"></i> Sistema de créditos na pulseira RFID</li>
+              <li><i class="fas fa-check"></i> Experiência personalizada para os visitantes</li>
+            </ul>
+          </div>
+        </section>
+    
+        <section class="cta">
+          <div class="container">
+            <h3>Experimente Agora</h3>
+            <p>Entre em contato para adquirir o sistema ArmillaEACS para o seu evento.</p>
+            <a href="#" class="btn">Entre em Contato</a>
+          </div>
+        </section>
+      </main>
+
+<footer>
+    <div class="container">
+      <p>&copy; 2023 ArmillaEACS - Early Access Control System</p>
+    </div>
+  </footer>
+  <script>
+    window.addEventListener('scroll', function() {
+        const scrollPosition = window.scrollY;
+        const target = document.querySelector('.scroll');
+        if (scrollPosition > 200) {
+            target.classList.add('js-scroll');
+        } else {
+            target.classList.remove('js-scroll');
+        }
+    });
+</script>
 
     <footer>
         ArmillaEACS &copy; 2023.
